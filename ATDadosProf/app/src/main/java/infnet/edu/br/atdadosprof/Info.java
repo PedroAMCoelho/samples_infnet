5ed13c92710da4b7c4fe4b77de9f9d694b85626d
PedroAMCoelho <pedrocoelhoprojetos@gmail.com>
2017-10-03 22:20:58 -0300

    Android App - Example of a Navigation Drawer
    student's professional info

---
+++ ATDadosProf/app/src/main/java/infnet/edu/br/atdadosprof/Info.java
@@ -0,0 +1,23 @@
+package infnet.edu.br.atdadosprof;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Info extends Fragment {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Informações Pessoais");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.info, container, false);
+    }
+}
---
+++ ATDadosProf/app/src/main/java/infnet/edu/br/atdadosprof/Pub.java
@@ -0,0 +1,23 @@
+package infnet.edu.br.atdadosprof;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Pub extends Fragment {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Publicações");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pub, container, false);
+    }
+}
